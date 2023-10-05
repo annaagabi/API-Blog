214532d0307b4a2e865ea642f29b353b37ed3113
Anna Gabriela <124202500+annaagabi@users.noreply.github.com>
2023-10-05 20:59:00 -0300

    Delete models directory

--- models/persons.js
+++
@@ -1,9 +0,0 @@
-const mongoose  = require('mongoose')
-
-const Person = mongoose.model('Person', { // Vai criar um tabela chamada Person
-    name: String,
-    salary: Number,
-    approved: Boolean,
-})
-
-module.exports = Person
